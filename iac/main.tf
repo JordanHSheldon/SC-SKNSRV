93bc942ca4a9001439a57d2e247ca7d79f6ee750
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-11-17 15:56:08 -0500

    added github actions workflow file and setup aws policies

--- iac/main.tf
+++ iac/main.tf
@@ -1,19 +1,5 @@
-variable "region" {
-    type = string
-    default = "us-east-1"
-}
-
-variable "vpc_cidr_range" {
-    type = string
-    default = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-    type = list(string)
-    default = ["10.0.0.0/24","10.0.1.0/24"]
-}
-
-variable "database_subnets" {
-    type = list(string)
-    default = ["10.0.8.0/24","10.0.9.0/24"]
+resource "aws_ssm_parameter" "foo" {
+  name = "foo"
+  type = "String"
+  value = "bar"
 }
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var cors = require('cors');
- 
+
 const app = express();
 const PORT = 3003;
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(cors(corsOptions));
 // Handling get request
 app.get('/', (res) => {
     res.send('hello world')
-  })
+})
  
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
