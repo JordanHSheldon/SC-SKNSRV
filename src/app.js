34ea5baed9195d68286abd61b1b00247db029b19
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-11-17 11:37:33 -0500

    added initial files for backend and iac

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+var cors = require('cors');
+ 
+const app = express();
+const PORT = 3003;
+app.use(express.json());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
+
+// Handling get request
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+ 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
