c700af52053d7182f07670a7e0a65adea4eb16a0
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-11-18 08:22:21 -0500

    added terraform modifications to fix deprecated backed provider

--- iac/main.tf
+++ iac/main.tf
@@ -1,5 +1,14 @@
 resource "aws_ssm_parameter" "foo" {
-  name = "foo"
-  type = "String"
+  name  = "foo"
+  type  = "String"
   value = "bar"
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "skincenter-terraform-tfstates"
+    key    = "network/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
