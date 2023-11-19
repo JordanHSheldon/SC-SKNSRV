15b7c678be10b4b04df5ed223c692a7786bb9ea0
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-11-19 11:59:20 -0500

    fck aws

--- iac/main.tf
+++ iac/main.tf
@@ -1,23 +1,29 @@
-provider "aws" {
-  region = "us-east-2"  # Specify your desired region here
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-unique-bucket-name"  # Replace with your preferred bucket name
-
-  tags = {
-    Name        = "ExampleBucket"
-    Environment = "Production"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-resource "aws_ssm_parameter" "example_parameter" {
-  name  = "example_parameter"
-  type  = "String"
-  value = "example_value"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-  tags = {
-    Environment = "Production"
-    Application = "ExampleApp"
-  }
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
 }
